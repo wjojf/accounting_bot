3cfcbafd6f977070b7eae4219b3d522e3a4a8a68
Tikhon Kozlov <71850641+wjojf@users.noreply.github.com>
2022-01-28 17:40:42 +0300

    fixed db_funcs errors

--- scripts/db_funcs.py
+++ scripts/db_funcs.py
@@ -15,15 +15,27 @@ CREATE_SPENDINGS_TABLE_QUERY = f'''CREATE TABLE IF NOT EXISTS {SPENDINGS_TABLE_N
                                                             )'''
 
 
-def join_dict_values(_dict):
+def join_dict_values(_dict, out_separator=','):
     joined = []
+
+    if len(_dict.keys()) == 1:
+        k = list(_dict.keys())[0]
+        v = list(_dict.values())[0]
+
+        if isinstance(v, str):
+            return '='.join([k, f"'{v}'"])
+        return '='.join([str(x) for x in [k,v]])
+
     for k, v in _dict.items():
         if isinstance(v, str):
             joined.append('='.join([k, f"'{v}'"]))
         else:
             joined.append('='.join([str(x) for x in [k,v]]))
 
-    return ','.join(joined)
+    return out_separator.join(joined)
+
+def get_result_of_query(cursor):
+    return cursor.fetchall()
 
 
 def create_conn(db_file):
@@ -66,9 +78,8 @@ def exec_select_query(table_connection, select_query):
     connection_cursor = table_connection.cursor()
 
     try:
-        rows = connection_cursor.execute(select_query)
-        return [row for row in rows]
-
+        connection_cursor.execute(select_query)
+        return connection_cursor.fetchall()
     except Exception as e:
         print('exec_select_query error')
         print(e)
@@ -77,15 +88,30 @@ def exec_select_query(table_connection, select_query):
 def generate_select_all_query(table_name):
     return f'SELECT * FROM {table_name}'
 
-def generate_select_query(table_name, *columns):
-    return f'''SELECT {columns} FROM {table_name}'''
+def generate_select_query(table_name, columns, where=None, distinct=False):
+    # handle columns
+    if len(columns) == 1:
+        columns_string = f'{columns[0]}'
+    else:
+        columns_string = ','.join(columns)
 
+
+
+    if where == None:
+        if distinct:
+            return f'''SELECT DISTINCT {columns_string} FROM {table_name}'''
+        return  f'''SELECT {columns_string} FROM {table_name}'''
+
+    where_joined = join_dict_values(where, ' AND ')
+    if distinct:
+        return f'''SELECT DISTINCT {columns_string} FROM {table_name} WHERE {where_joined}'''
+    return f'''SELECT {columns_string} FROM {table_name} WHERE {where_joined}'''
 
 def generate_update_query(table_name, update_values, where_values):
 
 
     update_joined = join_dict_values(update_values)
-    where_joined = join_dict_values(where_values)
+    where_joined = join_dict_values(where_values, ' AND ')
 
     return f'''
         UPDATE {table_name}
@@ -120,5 +146,60 @@ def exec_delete_query(table_connection, delete_query):
         connection_cursor.execute(delete_query)
     except Error as e:
         print(f'exec_delete_query -> {e}')
+
+
+# TODO: fix selecting multiple columns bug
+
+def get_unique_categories_by_user_id(table_connection,table_name, user_id):
+
+    unique_categories_query = generate_select_query(table_name,
+                                                    ['category'],
+                                                    where={'user_id': user_id},
+                                                    distinct=True)
+    #print(unique_categories_query)
+    categories = exec_select_query(table_connection, unique_categories_query)
+    categories =  [cat[0] for cat in categories]
+
+    unique_currenices_query = generate_select_query(
+        table_name,
+        ['currency'],
+        where={'user_id': user_id},
+        distinct=True
+    )
+    currencies = exec_select_query(table_connection, unique_currenices_query)
+    currencies = [curr[0] for curr in currencies]
+
+    # for each category
+        # for each currency
+            # sum price*ammount where user_id = user_id AND category = category AND currency = currency
+
+    output_dict = {}
+    for category in categories:
+
+        if category not in output_dict.keys():
+            output_dict[category] = {}
+
+        for currency in currencies:
+
+            if currency not in output_dict[category].keys():
+                output_dict[category][currency] = 0
+
+            price_ammount_query = generate_select_query(
+                table_name,
+                ('price', 'ammount'),
+                where={'user_id': user_id, 'category': category, 'currency': currency}
+            )
+            print(price_ammount_query)
+            price_ammount = exec_select_query(table_connection, price_ammount_query)
+
+            for record in price_ammount:
+                p, a = record
+                output_dict[category][currency] += p*a
+
+    return output_dict
+
+
+
+
 
 
--- scripts/test.py
+++ scripts/test.py
@@ -6,37 +6,58 @@ import datetime
 conn = create_conn(DB_FILEPATH)
 create_table(conn, CREATE_SPENDINGS_TABLE_QUERY)
 
-insertion_dict = {
-    'user_id': "last",
-    'date': str(datetime.datetime.now()),
-    'category': 'taxi',
-    'title': 'такси',
-    'price': 750,
-    'currency': 'RUB',
-    'ammount': 1
-}
-INSERTION_QUERY = generate_insert_query('spendings', insertion_dict)
-print(INSERTION_QUERY)
-exec_insert_query(conn, INSERTION_QUERY)
+insert_dicts = [
+    {
+        'user_id': "id_1",
+        'date': str(datetime.datetime.now()),
+        'category': 'taxi',
+        'title': 'такси',
+        'price': 750,
+        'currency': 'RUB',
+        'ammount': 1
+    },
 
-result = exec_select_query(conn, generate_select_all_query('spendings'))
-print(result)
+    {
+        'user_id': "wjojf",
+        'date': str(datetime.datetime.now()),
+        'category': 'food',
+        'title': 'гамбургер',
+        'price': 50,
+        'currency': 'RUB',
+        'ammount': 1
+    },
 
-update_values = {'user_id': 'new_last', 'price': 850}
-where_values = {'user_id': 'last'}
+    {
+        'user_id': 'wjojf',
+        'date': str(datetime.datetime.now()),
+        'category': 'games',
+        'title': 'minecraft',
+        'price': 299,
+        'currency': 'RUB',
+        'ammount': 1
+    },
 
+]
 
+for insert_dict in insert_dicts:
+    query = generate_insert_query('spendings', insert_dict)
+    exec_insert_query(conn, query)
 
-update_query = generate_update_query('spendings', update_values, where_values)
-print(update_query)
+print('AFTER FIRST INSERT')
+
+select_all_query = generate_select_all_query('spendings')
+rows = exec_select_query(conn, select_all_query)
+for row in rows:
+    print(*row)
+
+print('UPDATING SOME VALUES')
+
+update_query = generate_update_query('spendings', {'price': 75, 'ammount': 2},{'user_id': "wjojf", 'category': 'food'})
 exec_update_query(conn, update_query)
 
-result = exec_select_query(conn, generate_select_all_query('spendings'))
-print(result)
+rows = exec_select_query(conn, select_all_query)
+for row in rows:
+    print(*row)
 
-
-DELETE_ALL_QUERY = generate_delete_all_query('spendings')
-exec_delete_query(conn, DELETE_ALL_QUERY)
-
-result = exec_select_query(conn, generate_select_all_query('spendings'))
-print(result)
+print('GROUP BY MULTIPLE COLUMNS')
+print(get_unique_categories_by_user_id(conn,'spendings','wjojf'))
